6f16920ecac91e327e5aa2476f5083b3b8d9caee
Áquila Borges Moreira <aquila.brgs@gmail.com>
2026-03-10 23:45:15 -0300

    rename

--- src/app/features/tasks/usecases/index.ts
+++ src/app/features/tasks/usecases/index.ts
@@ -2,5 +2,5 @@ export * from './create-task.use-case';
 export * from './delete-tasks.use-case';
 export * from './list-tasks.use-case';
 export * from './update-task.use-case';
-export * from './load-task-panel-view-preferences.use-case';
-export * from './save-task-panel-view-preferences.use-case';
+export * from './load-task-panel-view-preferences-local-storage.use-case';
+export * from './save-task-panel-view-preferences-local-storage.use-case';
