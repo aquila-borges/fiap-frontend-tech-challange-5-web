e0a21ac3c55cf5762de15b3126c0c03cd0323b53
Áquila Borges Moreira <aquila.brgs@gmail.com>
2026-03-11 04:17:38 -0300

    empty list

--- src/app/features/tasks/components/task-empty-state-spotlight/task-empty-state-spotlight.component.ts
+++ src/app/features/tasks/components/task-empty-state-spotlight/task-empty-state-spotlight.component.ts
@@ -1,11 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-task-empty-state-spotlight',
   templateUrl: './task-empty-state-spotlight.component.html',
   styleUrl: './task-empty-state-spotlight.component.scss',
-  imports: [MatIconModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TaskEmptyStateSpotlightComponent {}
